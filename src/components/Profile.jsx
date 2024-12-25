323b5646a70a0e236a84c27b16745f58ff4bcb9d
mrphoenixuz <karimovmoff@gmail.com>
2024-12-25 12:39:41 +0500

    profile-update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material'
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
+import BasicModal from './Modal'
 
 const Profile = () => {
   const { user } = useSelector(state => state.auth)
@@ -13,14 +14,19 @@ const Profile = () => {
   }, [user])
   return (
     <div>
-      <Link to="/update-profile">Update Profile</Link>
-      <ul className='list-disc'>
-        <li>Username: {user?.username}</li>
-        <li>Gender: {user?.gender}</li>
-        <li>Email: {user?.email}</li>
-        <li>name: {user?.name}</li>
-        <li>age: {user?.age}</li>
-        <li>Image: <img src={user?.image ? `https://mustafocoder.pythonanywhere.com/api${user.image}` : "https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg"} alt="image" /></li>
+      <Link to="/update-profile"></Link>
+      <ul className='flex flex-col items-center justify-center'>
+        <div className='flex items-center justify-between gap-10 mt-4'>
+          <li><img className='w-60 h-60 rounded-full' src={user?.image ? `https://mustafocoder.pythonanywhere.com/api${user.image}` : "https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg"} alt="image" /></li>
+          <li className='text-5xl font-semibold uppercase'>{user?.username}</li>
+        </div>
+        <div className='mt-4'>
+          <li className='text-3xl flex justify-between gap-32'><p>Gender:</p> <span>{user?.gender}</span></li>
+          <li className='text-3xl flex justify-between gap-32'><p>Email:</p> <span>{user?.email}</span></li>
+          <li className='text-3xl flex justify-between gap-32'><p>Name:</p> <span>{user?.name}</span></li>
+          <li className='text-3xl flex justify-between gap-32'><p>Age:</p> <span>{user?.age}</span></li>
+        </div>
+        <BasicModal />
       </ul>
     </div>
   )
